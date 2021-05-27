5ee9c173b9faa07b6afa324d83f8208eab174557
Jonathan Mather <matherj@flip2.engr.oregonstate.edu>
2021-05-27 02:12:15 -0700

    checks for 'Fizz' now not 'Buzz'

--- test_FizzBuzz.py
+++ test_FizzBuzz.py
@@ -3,7 +3,7 @@ import unittest
 class testhw7(unittest.TestCase):
 
 	def test_hw7(self):
-		self.assertEqual(FizzBuzz.FB(arr[2]), "Buzz")
+		self.assertEqual(FizzBuzz.FB(arr[2]), "Fizz")
 
 if __name__ == '__main__':
 	unittest.main()
