8b16d7688e22a59ff111424dba94721c1a994a04
Jonathan Mather <matherj@flip1.engr.oregonstate.edu>
2021-06-04 23:06:23 -0700

    Wrote second test to test if years divisible by both 4 and 100 return as not leap years

--- test_LeapYear.py
+++ test_LeapYear.py
@@ -6,5 +6,8 @@ class testLeap(unittest.TestCase):
 	def test_leap(self):
 		self.assertEqual(LeapYear.leap(2012), 1)
 
+	def test_leap_1(self):
+		self.assertEqual(LeapYear.leap(1800), 0)
+
 if __name__ == '__main__':
 	unittest.main()
