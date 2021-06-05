4885d1fa2e5a5f5d6520eae95481473153f72cda
Jonathan Mather <matherj@flip1.engr.oregonstate.edu>
2021-06-04 23:10:02 -0700

    Wrote code to pass second test testing if years divisible by both 4 and 100 return as non-leap years

--- LeapYear.py
+++ LeapYear.py
@@ -1,8 +1,12 @@
 def leap(in_year):
 
 	if((in_year % 4) == 0):
-		print("%d is a leap year!" %in_year)
-		return 1
+		if((in_year % 100) == 0):
+			print("%d is not a leap year" %in_year)
+			return 0
+		else:
+			print("%d is a leap year!" %in_year)
+			return 1
 	else:
 		print("%d is not a leap year." %in_year)
 		return 0
